bcf91a467023bfabbe336091f0cc391d5d708561
Your Name <olubodekehinde2019@gmail.com>
2025-09-29 17:29:26 +0100

    latwst

--- src/Component/Homepage.js
+++ src/Component/Homepage.js
@@ -42,7 +42,7 @@ export default function Homepage() {
     <div>
         <p>🚀 Join 10,000+ Creators</p>
              <p>Turn Your Content Into Cash Instantly</p>
-                  <p>Creators on Pay2View are already earning by sharing exclusive PHOTOS, VIDEOS and PDF . Start monetizing your creativity today.</p>
+                  <p>Creators on Pay2View are already earning by sharing exclusive PHOTOS and PDF . Start monetizing your creativity today.</p>
                  
     </div>
     <div>
